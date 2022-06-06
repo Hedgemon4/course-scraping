e95c2a178515f05a64af4c8400dd1c4ddfc2bd68
Hedgemon4 <sethlinc4@gmail.com>
2022-06-06 11:54:53 -0600

    gets course names from Waterloo webpage

--- src/web-scraping-testing/waterlooWebScrapingTest.R
+++ src/web-scraping-testing/waterlooWebScrapingTest.R
@@ -2,6 +2,7 @@
 
 library(rvest)
 library(dplyr)
+library(stringr)
 
 
 # Webpage link to Waterloo Data Science Academic Calander
@@ -9,8 +10,26 @@ link = "http://ugradcalendar.uwaterloo.ca/page/MATH-Data-Science1"
 
 academicCalander = read_html(link)
 
+# Get webpage title
 title = academicCalander %>%
   html_node("title") %>%
   html_text()
 
+title <- str_squish(title)
+
 title
+
+# Get course codes
+courseCodes <- academicCalander %>%
+  html_nodes("#ctl00_contentMain_lblContent li a") %>%
+  html_text()
+
+courseCodes <- str_squish(courses)
+courseCodes
+
+# Get course names
+courseNames <- academicCalander %>%
+  html_nodes("#ctl00_contentMain_lblContent li") %>%
+  html_text()
+
+courseNames
