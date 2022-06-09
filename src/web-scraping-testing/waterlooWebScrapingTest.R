e0b4950e4d42469fe4a00e8a0ac6d9c7f4d66292
Hedgemon4 <sethlinc4@gmail.com>
2022-06-09 16:26:48 -0600

    formatted code for clarity and built function to remove excess information from course codes

--- src/web-scraping-testing/waterlooWebScrapingTest.R
+++ src/web-scraping-testing/waterlooWebScrapingTest.R
@@ -1,9 +1,34 @@
 # load required packages
-setwd("C:/Users/spenc/Documents/R/Projects/course-scraping/src/web-scraping-testing")
+# setwd("C:/Users/spenc/Documents/R/Projects/course-scraping/src/web-scraping-testing")
 library(rvest)
 library(dplyr)
 library(stringr)
-source("stringUtil.R")
+
+# functions
+get_courses <- function(course_description_link){
+  course_descriptions <- read_html(course_description_link) %>% html_nodes(".colspan-2:nth-child(4)") %>%
+    html_text() %>% str_squish() 
+  course_codes <- read_html(course_description_link) %>% html_nodes(".divTableCell:nth-child(1) strong") %>%
+    html_text() %>% remove_from_course_code() %>% str_squish()
+  course_names <- read_html(course_description_link) %>% html_nodes(".colspan-2 strong") %>%
+    html_text() %>% str_squish()
+  return(data.frame(course_codes, course_names, course_descriptions))
+}
+
+remove_from_course_code <- function(course_code){
+  course_code2 <- str_remove_all(course_code, "LEC|LAB|\\,|TST|TUT|0\\.50|PRJ|RDG|STU|0\\.25|0\\.00|2\\.50")
+  return(course_code2)
+}
+
+remove_from_string <- function(a, b){
+  i = 1
+  for(val in a){
+    remove <- paste(b[i], " ", sep = "")
+    a[i] <- str_remove(val, remove)
+    i <- i + 1
+  }
+  return(a)
+}
 
 # Webpage link to Waterloo Data Science Academic Calendar
 academicCalendar = read_html("http://ugradcalendar.uwaterloo.ca/page/MATH-Data-Science1")
@@ -28,28 +53,19 @@ courseNames <- remove_from_string(courseNamesWithCodes, courseCodes)
 
 # Create data frame with course codes and names
 courses = data.frame(courseCodes, courseNames)
-colnames(courses) <- c('Course Number', 'Course Name')
+colnames(courses) <- c('Course Code', 'Course Name')
 
 # Display Data Frame
-courses['Course Description'] <- NA
 View(courses)
 
 # TODO: Get course descriptions from course links
 
-# Get course desrciption
-get_courses <- function(course_description_link){
-  course_descriptions <- read_html(course_description_link) %>% html_nodes(".colspan-2:nth-child(4)") %>%
-    html_text()
-  course_codes <- read_html(course_description_link) %>% html_nodes(".divTableCell:nth-child(1) strong") %>%
-    html_text()
-  course_names <- read_html(course_description_link) %>% html_nodes(".colspan-2 strong") %>%
-    html_text()
-  return(data.frame(course_codes, course_names, course_descriptions))
-}
-
+# Get course desrciptions
 compsci_courses <- get_courses("http://ugradcalendar.uwaterloo.ca/courses/CS")
 View(compsci_courses)
 math_courses <- get_courses("http://ugradcalendar.uwaterloo.ca/courses/MATH")
 View(math_courses)
 stat_courses <- get_courses("http://ugradcalendar.uwaterloo.ca/courses/STAT")
 View(stat_courses)
+
+# TODO: Get "one of", "all of", and other labels from requirements
