ebef5b87664bc61f7f168f880ff784d9abd833c6
Hedgemon4 <sethlinc4@gmail.com>
2022-06-10 09:11:17 -0600

    reset database

--- src/database-testing/databaseTest.py
+++ src/database-testing/databaseTest.py
@@ -7,9 +7,3 @@ cursor = connection.cursor()
 # df = pandas.read_csv("C:/Users/spenc/PycharmProjects/course-scraping/src/web-scraping-testing/compsci_courses.csv")
 
 # df.to_sql("Computer Science Courses", connection)
-
-cursor.execute("SELECT * FROM 'Computer Science Courses' WHERE course_codes ='CS 136' ")
-
-connection.commit()
-
-print(cursor.fetchall())
