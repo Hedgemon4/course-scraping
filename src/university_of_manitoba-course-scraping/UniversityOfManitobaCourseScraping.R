261ba00998cbf944b447fbba1e98494e23fc5945
Hedgemon4 <sethlinc4@gmail.com>
2022-06-30 11:29:14 -0700

    Built loop to gather course information

--- src/university_of_manitoba-course-scraping/UniversityOfManitobaCourseScraping.R
+++ src/university_of_manitoba-course-scraping/UniversityOfManitobaCourseScraping.R
@@ -12,6 +12,8 @@ source("~/R/Projects/course-scraping/src/util/CourseScrapingUtil.R")
 # TODO List ####
 # TODO: Figure out how to seperate information in table on main page
 # TODO: Get course information
+# TODO: Watch video about dplyr
+# TODO: Use course lists to get categories and category requirements
 
 # Program Requirements ####
 web_link <- "https://catalog.umanitoba.ca/undergraduate-studies/science/data-science/data-science-bsc-major"
@@ -26,39 +28,36 @@ course_nodes <- html_nodes(web_page, "#degreerequirementstextcontainer .code")
 
 courses_main_link <- "https://catalog.umanitoba.ca/"
 course_info_links <- html_nodes(web_page, "#degreerequirementstextcontainer .code") %>% html_attr("href")
+number_of_courses <- length(course_info_links)
 
-course_description <- vector(mode = "character", length = length(course_info_links))
-prereq_and_coreq <- vector(mode = "character", length = length(course_info_links))
-coreq <- vector(mode = "character", length = length(course_info_links))
-attributes <- vector(mode = "character", length = length(course_info_links))
-equiv <- vector(mode = "character", length = length(course_info_links))
-exclusive <- vector(mode = "character", length = length(course_info_links))
+course_name <- vector(mode = "character", length = number_of_courses)
+course_description <- vector(mode = "character", length = number_of_courses)
+course_credit <- vector(mode = "character", length = number_of_courses)
+prereq <- vector(mode = "character", length = number_of_courses)
+coreq <- vector(mode = "character", length = number_of_courses)
+attributes <- vector(mode = "character", length = number_of_courses)
+equiv <- vector(mode = "character", length = number_of_courses)
+exclusive <- vector(mode = "character", length = number_of_courses)
 
 i <- 1
 for(item in course_info_links){
   course_link <- paste0(courses_main_link, course_info_links[i])
-  course_information <- read_html(course_link) %>% html_nodes(".courseblockextra") %>% html_text() %>% str_squish()
+  course_page <- read_html(course_link)
+  course_information <- html_nodes(course_page, ".courseblockextra") %>% html_text() %>% str_squish()
+  course_name[i] <- html_node(course_page, ".detail-title strong") %>% html_text() %>% str_squish()
   course_description[i] <- course_information[1]
+  course_credit[i] <- html_node(course_page, ".detail-hours_html strong") %>% html_text() %>% str_squish()
   equiv[i] <- paste(grep("Equiv To:", course_information, value = TRUE), collapse = "")
   exclusive[i] <- paste(grep("Mutually Exclusive:", course_information, value = TRUE), collapse = "")
   attributes[i] <- paste(grep("Attributes:", course_information, value = TRUE), collapse = "")
-  prereq[i] <- grep("PR/CR", course_information, value = TRUE) %>% paste(collapse = "") %>%
-    sub("(.*)(Prerequisite.*)", "\\2", .)
-  coreq[i] <- grep("PR/CR", course_information, value = TRUE) %>% paste(collapse = "") %>%
-    sub("(.*)(Co-requisite.*)", "\\2", .)
+  prcr <- grep("PR/CR", course_information, value = TRUE) %>% strsplit(split = "\\.") %>% unlist()
+  prereq[i] <- grep("Prerequisite(s|)", prcr, ignore.case = TRUE, value = TRUE) %>% paste(collapse = "")
+  coreq[i] <- grep("Co-requisite|corequisite|co requisite", prcr, ignore.case = TRUE, value = TRUE) %>% paste(collapse = "")
   i = i + 1
 }
 
-course_info_test <- read_html(paste0(courses_main_link, course_info_links[8])) %>% html_nodes(".courseblockextra") %>% html_text() %>% str_squish()
-
-item <- grep("PR/CR", course_info_test, value = TRUE) %>% paste(collapse = "") 
-
-prereq_test <- sub("(.*?)(Prerequisite.*){1}(Co-requisite.*){1}", "\\1", item)
-
-course_info <- data.frame(course_codes, course_description, equiv, exclusive, attributes, prereq, coreq)
+course_info <- data.frame(course_codes, course_name, course_description, course_credit, prereq, coreq, equiv, exclusive, attributes)
 
 information <- html_nodes(web_page, "#degreerequirementstextcontainer .codecol") %>% html_text()
 
 program_table <- html_nodes(web_page, "#degreerequirementstextcontainer > table.sc_plangrid") %>% html_table() %>% .[[1]]
-
-course_names <- html_elements(web_page, xpath = "//*[@id=\"degreerequirementstextcontainer\"]/table/tr/td/text()") %>% html_text()
