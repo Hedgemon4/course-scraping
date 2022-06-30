de0f3416ff0f1b28967e57ee3e9aebe070c065a5
Hedgemon4 <sethlinc4@gmail.com>
2022-06-30 09:53:20 -0700

    Continued working on gathering course requirements

--- src/university_of_manitoba-course-scraping/UniversityOfManitobaCourseScraping.R
+++ src/university_of_manitoba-course-scraping/UniversityOfManitobaCourseScraping.R
@@ -27,10 +27,8 @@ course_nodes <- html_nodes(web_page, "#degreerequirementstextcontainer .code")
 courses_main_link <- "https://catalog.umanitoba.ca/"
 course_info_links <- html_nodes(web_page, "#degreerequirementstextcontainer .code") %>% html_attr("href")
 
-course_info_test <- read_html(paste0(courses_main_link, course_info_links[1])) %>% html_nodes(".courseblockextra") %>% html_text() %>% str_squish()
-
 course_description <- vector(mode = "character", length = length(course_info_links))
-prereq <- vector(mode = "character", length = length(course_info_links))
+prereq_and_coreq <- vector(mode = "character", length = length(course_info_links))
 coreq <- vector(mode = "character", length = length(course_info_links))
 attributes <- vector(mode = "character", length = length(course_info_links))
 equiv <- vector(mode = "character", length = length(course_info_links))
@@ -44,11 +42,20 @@ for(item in course_info_links){
   equiv[i] <- paste(grep("Equiv To:", course_information, value = TRUE), collapse = "")
   exclusive[i] <- paste(grep("Mutually Exclusive:", course_information, value = TRUE), collapse = "")
   attributes[i] <- paste(grep("Attributes:", course_information, value = TRUE), collapse = "")
+  prereq[i] <- grep("PR/CR", course_information, value = TRUE) %>% paste(collapse = "") %>%
+    sub("(.*)(Prerequisite.*)", "\\2", .)
+  coreq[i] <- grep("PR/CR", course_information, value = TRUE) %>% paste(collapse = "") %>%
+    sub("(.*)(Co-requisite.*)", "\\2", .)
   i = i + 1
 }
 
-course_info <- data.frame(course_codes, course_description, equiv, exclusive, attributes)
+course_info_test <- read_html(paste0(courses_main_link, course_info_links[8])) %>% html_nodes(".courseblockextra") %>% html_text() %>% str_squish()
 
+item <- grep("PR/CR", course_info_test, value = TRUE) %>% paste(collapse = "") 
+
+prereq_test <- sub("(.*?)(Prerequisite.*){1}(Co-requisite.*){1}", "\\1", item)
+
+course_info <- data.frame(course_codes, course_description, equiv, exclusive, attributes, prereq, coreq)
 
 information <- html_nodes(web_page, "#degreerequirementstextcontainer .codecol") %>% html_text()
 
