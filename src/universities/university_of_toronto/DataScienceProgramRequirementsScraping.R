e71ac6a0efdbe17c1db4945eba3327a3373fa267
Hedgemon4 <sethlinc4@gmail.com>
2022-07-05 16:35:47 -0700

    Finished gathering course information for required courses

--- src/universities/university_of_toronto/DataScienceProgramRequirementsScraping.R
+++ src/universities/university_of_toronto/DataScienceProgramRequirementsScraping.R
@@ -11,6 +11,10 @@ library(stringi)
 # Source for util functions
 source("~/R/Projects/course-scraping/src/util/CourseScrapingUtil.R")
 
+# TODO List ####
+# TODO: Figure out how to separate courses into categories
+# TODO: Scrape category requirements
+
 # Required Courses ####
 program_link <-
   "https://artsci.calendar.utoronto.ca/program/asspe1687"
@@ -28,46 +32,129 @@ academic_calendar_link <- "https://artsci.calendar.utoronto.ca"
 
 number_of_courses <- length(courses)
 
-course_codes <- vector(mode = "character", length = number_of_courses)
-course_names <- vector(mode = "character", length = number_of_courses)
-course_descriptions <- vector(mode = "character", length = number_of_courses)
+course_codes <-
+  vector(mode = "character", length = number_of_courses)
+course_names <-
+  vector(mode = "character", length = number_of_courses)
+course_descriptions <-
+  vector(mode = "character", length = number_of_courses)
 prereq <- vector(mode = "character", length = number_of_courses)
 coreq <- vector(mode = "character", length = number_of_courses)
 breadth <- vector(mode = "character", length = number_of_courses)
-hours <- vector(mode = "character", length = number_of_courses)
 antireq <- vector(mode = "character", length = number_of_courses)
-distribution <- vector(mode = "character", length = number_of_courses)
+distribution <-
+  vector(mode = "character", length = number_of_courses)
 delivery <- vector(mode = "character", length = number_of_courses)
 antireq <- vector(mode = "character", length = number_of_courses)
-recommended <- vector(mode = "character", length = number_of_courses)
+recommended <-
+  vector(mode = "character", length = number_of_courses)
+credits <- vector(mode = "character", length = number_of_courses)
+hours <- vector(mode = "character", length = number_of_courses)
 other <- vector(mode = "character", length = number_of_courses)
 
 i <- 1
-for(link in course_links) {
+for (link in course_links) {
   course_page <- read_html(paste0(academic_calendar_link, link))
   course_title <-
     html_nodes(course_page, ".page-title") %>% html_text() %>%
     str_split(":") %>% unlist() %>% str_squish()
-  course_codes[i] <- course_title[1]
-  course_names[i] <- course_title[2]
+  course_codes[i] <- course_title[1] %>% str_squish()
+  course_names[i] <- course_title[2] %>% str_squish()
   course_descriptions[i] <-
     html_nodes(course_page, "#block-fas-content .field--label-hidden p") %>%
     html_text() %>% str_squish() %>% paste(collapse = " ")
-  other_course_information <- html_nodes(course_page, "#block-fas-content > div > article > div > div") %>%
+  other_course_information <-
+    html_nodes(course_page, "#block-fas-content > div > article > div > div") %>%
     html_text() %>% str_squish()
-  other_course_information <- other_course_information[! other_course_information %in% course_descriptions[i]]
-  prereq[i] <- grep("Prerequisite", other_course_information, value = TRUE) %>% paste(collapse = " ")
-  coreq[i] <- grep("Corequisite", other_course_information, value = TRUE) %>% paste(collapse = " ")
-  breadth[i] <- grep("Breadth Requirements", other_course_information, value = TRUE) %>% paste(collapse = " ")
-  hours[i] <- grep("Hours", other_course_information, value = TRUE) %>% paste(collapse = " ")
-  distribution[i] <- grep("Distribution Requirements", other_course_information, value = TRUE) %>% paste(collapse = " ")
-  delivery[i] <- grep("Mode of Delivery", other_course_information, value = TRUE) %>% paste(collapse = " ")
-  antireq[i] <- grep("Exclusion", other_course_information, value = TRUE) %>% paste(collapse = " ")
-  recommended[i] <- grep("Recommended Preparation", other_course_information, value = TRUE) %>% paste(collapse = " ")
-  other[i] <- grep("Prerequisite|Corequisite|Breadth Requirements|Hours|Distribution Requirements|Hours|Mode of Delivery|Exclusion|Recommended Preparation",
-                   other_course_information, value = TRUE, invert = TRUE) %>% paste(collapse = " ")
+  other_course_information <-
+    other_course_information[!other_course_information %in% course_descriptions[i]]
+  prereq[i] <-
+    grep("Prerequisite", other_course_information, value = TRUE) %>% paste(collapse = " ")
+  coreq[i] <-
+    grep("Corequisite", other_course_information, value = TRUE) %>% paste(collapse = " ")
+  breadth[i] <-
+    grep("Breadth Requirements", other_course_information, value = TRUE) %>% paste(collapse = " ")
+  distribution[i] <-
+    grep("Distribution Requirements",
+         other_course_information,
+         value = TRUE) %>% paste(collapse = " ")
+  delivery[i] <-
+    grep("Mode of Delivery", other_course_information, value = TRUE) %>% paste(collapse = " ")
+  antireq[i] <-
+    grep("Exclusion", other_course_information, value = TRUE) %>% paste(collapse = " ")
+  recommended[i] <-
+    grep("Recommended Preparation", other_course_information, value = TRUE) %>% paste(collapse = " ")
+  if (grepl("(CSC|JSC|MAT|STA)([0-9]*)(H)([0-9]*)", course_codes[i])) {
+    credits[i] <- "0.5"
+  } else if (grepl("(CSC|JSC|MAT|STA)([0-9]*)(Y)([0-9]*)", course_codes[i])) {
+    credits[i] <- "1.0"
+  }
+  hours[i] <-
+    grep("Hours", other_course_information, value = TRUE) %>% paste(collapse = " ")
+  other[i] <-
+    grep(
+      "Prerequisite|Corequisite|Breadth Requirements|Hours|Distribution Requirements|Hours|Mode of Delivery|Exclusion|Recommended Preparation",
+      other_course_information,
+      value = TRUE,
+      invert = TRUE
+    ) %>% paste(collapse = " ")
   i <- i + 1
 }
 
-required_courses <- data.frame(course_codes, course_names, course_descriptions, prereq, coreq, antireq, recommended, breadth, delivery, distribution, hours, other) %>%
+lecture <- grepl("L", hours)
+tutorial <- grepl("T", hours)
+lab <- grepl("P", hours)
+seminar <- grepl("S", hours)
+
+# Clean Data
+antireq <- str_remove_all(antireq, "Exclusion |NOTE: ")
+coreq <- str_remove_all(coreq, "Corequisite ")
+prereq <- str_remove_all(prereq, "Prerequisite ")
+hours <- str_remove_all(hours, "Hours ")
+delivery <- str_remove_all(delivery, "Mode of Delivery ")
+breadth <- str_remove_all(breadth, "Breadth Requirements ")
+distribution <-
+  str_remove_all(distribution, "Distribution Requirements ")
+
+required_courses <-
+  data.frame(
+    course_codes,
+    course_names,
+    course_descriptions,
+    credits,
+    antireq,
+    coreq,
+    prereq,
+    recommended,
+    hours,
+    lecture,
+    lab,
+    tutorial,
+    seminar,
+    breadth,
+    delivery,
+    distribution,
+    other
+  ) %>%
   filter(!course_codes == "Sorry, this course is not in the current Calendar.")
+
+colnames(required_courses) <-
+  c(
+    "Course Code",
+    "Course Name",
+    "Course Description",
+    "Credit Amount",
+    "Antirequisite",
+    "Corequisite",
+    "Prerequisite",
+    "Recommended Courses",
+    "Hours",
+    "Lecture",
+    "Lab",
+    "Tutorial",
+    "Seminar",
+    "Breadth Requirement",
+    "Delivery Format",
+    "Distribution Requirement",
+    "Note"
+  )
