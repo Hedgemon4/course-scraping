07caaa53d4526a4b905157b622b3be49e7b11dea
Hedgemon4 <sethlinc4@gmail.com>
2022-07-16 18:19:10 -0700

    Got webpage for program requirements

--- src/universities/wilfrid_laurier_university/ProgramRequirementScraping.R
+++ src/universities/wilfrid_laurier_university/ProgramRequirementScraping.R
@@ -1,1 +1,14 @@
-# Program Requirement Scraping for Wilfrid Laurier University
+# Program Requirement Scraping for Wilfrid Laurier University ####
+
+# Load required packages
+library(rvest)
+library(dplyr)
+library(stringr)
+library(stringi)
+library(tidyverse)
+
+# Source for functions
+source("~/R/Projects/course-scraping/src/util/CourseScrapingUtil.R")
+
+program_link <- "https://academic-calendar.wlu.ca/program.php?cal=1&d=2589&p=5763&s=1034&y=85"
+program_page <- read_html(program_link)
