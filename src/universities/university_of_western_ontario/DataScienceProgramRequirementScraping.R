686a6323f6bdce59a34452e30e6637fb55d92fb0
Hedgemon4 <sethlinc4@gmail.com>
2022-08-05 12:02:06 -0600

    Started scraping program requirements

--- src/universities/university_of_western_ontario/DataScienceProgramRequirementScraping.R
+++ src/universities/university_of_western_ontario/DataScienceProgramRequirementScraping.R
@@ -25,3 +25,40 @@ program_info <- html_nodes(program_page, ".moduleInfo p") %>%
     .,
     value = TRUE
   )
+
+category_credit_amount <-
+  str_extract_all(program_info, "([0-9]\\.[0-9])") %>% unlist() %>%
+  as.numeric()
+category_credit_amount <- category_credit_amount * 6
+
+requirement_category <- vector(mode = "character")
+category_description <- vector(mode = "character")
+min_credit <- vector(mode = "numeric")
+max_credit <- vector(mode = "numeric")
+isCore <- vector(mode = "logical")
+
+alphabets <- c("A", "B", "C", "D", "E", "F", "G")
+
+test <- str_split(program_info[2], ",")
+
+i <- 1
+k <- 1
+for (item in program_info) {
+  requirement_category[i] <- paste0("Category G", k)
+  category_description[i] <-
+    str_extract(item, "(?<=:)(.*)") %>% str_squish()
+  courses_in_category <-
+    str_extract_all(
+      item,
+      "((Computer\\sScience)|(Data\\sScience)|(Mathematics)|(Software\\sEngineering)|(Statistical\\sSciences))\\s[0-9]{4}([A-Z](\\/)?)*"
+    ) %>% unlist()
+  if (grepl("(\\()|(\\))", item)) {
+    subcategories <- str_split(item, ",")
+  for(sub in subcategories){
+    
+  }
+    requirement_category[i] <- paste0("Category G", k)
+  }
+  k <- k + 1
+  i <- i + 1
+}
